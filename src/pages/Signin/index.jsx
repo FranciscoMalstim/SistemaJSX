137e57636fd2a271c7af002daeb4a5354b2ffd52
FranciscoMalstim <franciscosantos2501@gmail.com>
2022-09-09 11:13:41 -0300

    login screen creation

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react' 
+import { Link } from 'react-router-dom'
+
+import logo from '../../assets/medicamento.png'
+import './signin.css'
 function SignIn() {
- 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Clicou')
+  }
 
   return (
-    <div className="App">
-      <h1>Pagina de Login</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo Sistema"  />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder='Digite seu email'value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder='Digite sua Senha'value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type='submit'>Acessar</button>
+        </form>
+        
+        <Link to="/register">Criar sua conta</Link>
+        
+      </div>
     </div>
   )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/SignUp';
@@ -8,6 +9,8 @@ export default function RoutesFilter(){
     <Routes>
       <Route  path="/" element={<SignIn />}/>
       <Route  path="/register" element={<SignUp />}/>
+
+      <Route path='/dashboard' element={<Dashboard />} isPrivate/>
     </Routes>
   )
 }
